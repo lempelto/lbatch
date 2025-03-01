8e7ee839cfcddcddd0d9ea7ce5a8d690a8fa371a
Aku Lempelto <107255997+lempelto@users.noreply.github.com>
2025-03-01 23:45:08 +0200

    Development, 02/2025:

--- lbatch.py
+++ lbatch.py
@@ -1,9 +1,39 @@
 import os.path as path
-import sys
-import argparse
 import subprocess
 
-def writesh(pyFile: str, cluster: str="", dft: str="", np: int=0, ntsk: int=0, nnd: int=0, ppnd: int=0, tskpnd: int=0, pptsk: int=1,\
+
+vasp_fast = {
+    "GGA": "PE",
+    "ALGO": "Normal",
+    "ENCUT": 400.,
+    "PREC": "Accurate",
+    "LASPH": True,
+    "LREAL": "Auto",
+    "IVDW": 12,
+    "EDIFF": 1.0e-05,
+    "ISMEAR": 1,
+    "SIGMA": 0.05,
+    "ISPIN": 1,
+    "NELMIN": 8,
+    "NELM": 150,
+    "NELMDL": -8,
+    "ISYM": 0,
+    "IBRION": 2,
+    "EDIFFG": -1.0e-02,
+    "NSW": 500,
+    "LDAU": True,
+    "LDAUTYPE": 2,
+    "LDAUL": "2 -1 2 -1 -1",
+    "LDAUU": "6.20 0.00 4.40 0.00 0.00",
+    "LDAUJ": "0.00 0.00 0.00 0.00 0.00",
+    "LMAXMIX": 6,
+    # "KPAR": 4,
+    "LORBIT": 11,
+    "LCHARG": True,
+    "LWAVEH5": True
+}
+
+def write_sh(pyFile: str, cluster: str="", dft: str="", np: int=0, ntsk: int=0, nnd: int=0, ppnd: int=0, tskpnd: int=0, pptsk: int=1,\
     mpp: int=0, e_notif: str="", account: str="", partition: str="", timelimit: str="") -> str:
     """Creates a Slurm script file that runs the specified script with gpaw-python
     pyFile: Python script to run. Required
@@ -113,7 +143,7 @@ def writesh(pyFile: str, cluster: str="", dft: str="", np: int=0, ntsk: int=0, n
     return shFile
 
 
-def writepy(inFile: str="", dft: str="", tem: str="~/pytem.py") -> str:
+def write_py(inFile: str="", dft: str="", tem: str="~/pytem.py") -> str:
     """Writes Python input script to run a job with ASE"""
     inExt = ""
 
@@ -128,8 +158,8 @@ def writepy(inFile: str="", dft: str="", tem: str="~/pytem.py") -> str:
         else:
             name = inFile
         
-        """ If someone's sent a Python file as molecular coordinates, let's stop so that
-            we don't accidentally overwrite it """
+        # If someone's sent a Python file as molecular coordinates,
+        # let's stop so that we don't accidentally overwrite it
         if inExt == "py": raise TypeError
         
         pyFile = name + ".py"
@@ -139,8 +169,10 @@ def writepy(inFile: str="", dft: str="", tem: str="~/pytem.py") -> str:
         try:
             with open(tem, "r") as puh:
                 for l in puh:
-                    if l.__contains__("?name?"): l = l.replace("?name?", str(name))
-                    if l.__contains__("?inFile?"): l = l.replace("?inFile?", str(inFile))
+                    if l.__contains__("?name?"):
+                        l = l.replace("?name?", str(name))
+                    if l.__contains__("?inFile?"):
+                        l = l.replace("?inFile?", str(inFile))
                     pytxt = pytxt + l
         except FileNotFoundError:
             pytxt = f"The template file was not found in {tem}"
@@ -153,8 +185,28 @@ def writepy(inFile: str="", dft: str="", tem: str="~/pytem.py") -> str:
         return "_na"
 
 
+def write_vasp(inFile: str="") -> str:
+    """Writes input files for a Vasp calculation"""
+    _incar = "INCAR"
+    return _incar
+
+
+
 if __name__ == "__main__":
-    cluster = "Mahti"
+    import platform
+    import argparse
+
+
+    # Try to guess which supercomputer we're on.
+    # This can be overwritten by command line arguments
+    hostname = platform.node()
+    if hostname.__contains__("mahti"):
+        cluster = "Mahti"
+    elif hostname.__contains__("puhti"):
+        cluster = "Puhti"
+    else:
+        cluster = "Mahti"
+
     account = "project_2012891"
     dft = "VASP"
     lbfolder = path.dirname(path.realpath(__file__))
@@ -162,32 +214,65 @@ if __name__ == "__main__":
     vtem = path.abspath(lbfolder + "/vasp_tem.py")
     nebtem = path.abspath(lbfolder + "/nebtem.py")
     
-    arpar = argparse.ArgumentParser(description="Creates a SLURM script to run a specified file")
+    arpar = argparse.ArgumentParser(prog="LBatch", 
+                                    description="Creates the necessary files \
+                                        to run a DFT calculation through SLURM")
 
-    arpar.add_argument("file", type=str, help="The file containing the atomic coordinates")
-    arpar.add_argument("-v", action="store_true", help="Verbose. By default lbatch returns only the name of the SLURM script file")
+    arpar.add_argument("file", type=str, 
+                       help="The file containing the atomic coordinates")
+    arpar.add_argument("-v", action="store_true", 
+                       help="Verbose. By default lbatch returns only the name \
+                            of the SLURM script file")
     arpar.add_argument("-d", metavar="DFT CODE", type=str, default=dft,
-                        help=f"Which DFT package to run the calculations using. Currently the options are GPAW and VASP. Defaults to \"{cluster}\"")
+                        help=f"Which DFT package to run the calculations with.\
+                            Currently the options are GPAW and VASP. Defaults \
+                            to \"{cluster}\"")
     arpar.add_argument("-c", metavar="CLUSTER", type=str, default=cluster, 
-                        help=f"The name of the cluster the job will be run on. This allows the code to generate suitable default values. Defaults to \"{cluster}\"")
+                        help=f"The name of the cluster the job will be run on.\
+                            This allows the code to generate suitable default \
+                            values. Defaults to \"{cluster}\"")
     arpar.add_argument("-a", metavar="ACCOUNT", type=str, default=account,
-                        help=f"The account used for computer time when running on CSC supercomputers. Defaults to \"{account}\"")
-    arpar.add_argument("-p", type=int, default=0, help="Number of processors")
-    arpar.add_argument("-n", type=int, default=0, help="Number of nodes")
-    arpar.add_argument("-m", type=float, default=0., help="Memory per processor (GB)")
-    arpar.add_argument("-t", type=int, default=0, help="Time limit in hours (int). There's no sanity checking here so check the limit for each cluster")
-    arpar.add_argument("-s", action="store_true", help="Automatically submits the job to the SLURM system")
-    arpar.add_argument("--sin", action="store_true", help="Loads the 'Python singularity' containing scipy, pandas, matplotlib etc. DON'T USE THIS")
-    arpar.add_argument("--ppnd", type=int, default=0, help="Processors per node")
-    arpar.add_argument("--part", metavar="PARTITION", type=str, default="", help="The SLURM partition to use. Defaults to \"medium\" on Mahti and \"large\" on Puhti")
-    arpar.add_argument("--email", type=str, default="", help="You can give an email address to recieve a notification of events during the job's execution")
-    arpar.add_argument("--sh", action="store_true", help="Generates ONLY the SLURM script file. \"file\" should then contain the script to be run, eg. a python file")
-    arpar.add_argument("--neb", action="store_true", help="Generates a file to run a Nudged Elastic Band (NEB) calculation")
+                        help=f"The account used for computer time when running\
+                            on CSC supercomputers. Defaults to \"{account}\"")
+    arpar.add_argument("-p", type=int, default=0,
+                        help="Number of processors")
+    arpar.add_argument("-n", type=int, default=0, 
+                       help="Number of nodes")
+    arpar.add_argument("-m", type=float, default=0., 
+                       help="Memory per processor (GB)")
+    arpar.add_argument("-t", type=int, default=0, 
+                       help="Time limit in hours (int). There's no sanity \
+                            checking here so check the limit for each cluster")
+    arpar.add_argument("-s", action="store_true",
+                       help="Automatically submit the job to the SLURM system")
+    arpar.add_argument("--ase", action="store_true", 
+                       help="Uses ASE the ASE/Python interface to the DFT \
+                            calculator. Enabled automatically when DFT CODE = \
+                            \"GPAW\"")
+    arpar.add_argument("--email", type=str, default="", 
+                       help="You can give an email address to recieve a \
+                        notification of events during the job's execution")
+    arpar.add_argument("--sin", action="store_true",
+                       help="Loads the 'Python singularity' containing scipy, \
+                            pandas, matplotlib etc.")
+    arpar.add_argument("--ppnd", type=int, default=0, 
+                       help="Processors per node")
+    arpar.add_argument("--part", metavar="PARTITION", type=str, default="", 
+                       help="The SLURM partition to use. Defaults to \
+                            \"medium\" on Mahti and \"large\" on Puhti")
+    arpar.add_argument("--sh", action="store_true", 
+                       help="Generates ONLY the SLURM script file. \"file\" \
+                            should then contain the script to be run, eg. a \
+                            python file")
+    arpar.add_argument("--neb", action="store_true", 
+                       help="Generates a file to run a Nudged Elastic Band \
+                            (NEB) calculation using ASE")
 
     args = arpar.parse_args()
 
     fil = args.file
     dft = args.d.upper()
+    use_ase = args.ase
     verbose = args.v
     lazy = not args.sh
 
@@ -197,43 +282,63 @@ if __name__ == "__main__":
     else:
         timelimit = ""
     
-    if dft == "GPAW":
-        if args.neb: 
-            pytem = nebtem
-        else:
-            pytem = gtem
+
+    # Should not include the srun / mpirun command yet
+    run_cmd = ""
+
+    if args.neb:
+        pytem = nebtem
+    elif dft == "GPAW":
+        use_ase = True
+        run_cmd = "gpaw-python "
+        pytem = gtem
     elif dft == "VASP":
-        pytem = vtem
-    else: raise NotImplemented
+        if use_ase:
+            run_cmd = "python3 "
+            pytem = vtem
+        else:
+            run_cmd = "vasp_std"
+
     
-    """ Lazy mode writes the Python file to run a job using ASE on the user's behalf.
-        I'm lazy so I've set it as default. The mode is disabled using the --sh flag """
+    # Lazy mode writes the Python file for ASE on the user's behalf.
+    # I'm lazy so it's set as default. Can be disabled by using --sh
     if lazy:
-        if verbose: print("Python file:")
-        _fil = writepy(inFile=fil, dft=dft, tem=pytem)
-        if _fil == "_na":
-            print(f"No file \"{fil}\" found")
-        elif verbose:
-            print(_fil)
-            print("It is recommended that you check the parameters before submitting")
-        fil = _fil
+        if use_ase:
+            if verbose: print("Python file:")
+            _fil = write_py(inFile=fil, dft=dft, tem=pytem)
+            if _fil == "_na":
+                print(f"No file \"{fil}\" found")
+            elif verbose:
+                print(_fil)
+                print("It is recommended that you check the parameters before \
+                      submitting")
+            run_cmd += fil
+        elif dft == "VASP":
+            _fil = write_vasp(inFile=fil)
+        else:
+            raise NotImplemented
+
+    fil = path.abspath(_fil)
 
 
-    fil = path.abspath(fil)
-    
     if path.exists(fil):
-        sph = writesh(pyFile=fil, cluster=args.c, dft=dft, np=args.p, nnd=args.n, ppnd=args.ppnd, mpp=args.m,
-                      e_notif=args.email, account=args.a, partition=args.part, timelimit=timelimit)
+        sph = write_sh(runcmd=run_cmd, cluster=args.c, dft=dft,
+                       e_notif=args.email, account=args.a, partition=args.part,
+                       np=args.p, nnd=args.n, ppnd=args.ppnd, mpp=args.m,
+                       timelimit=timelimit)
         if verbose: print("SLURM batch file:")
-
         if args.s:
-            sproc = subprocess.run(f"sbatch {sph}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            sproc = subprocess.run(f"sbatch {sph}",
+                                   shell=True,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT)
             sprout = sproc.stdout.decode()
             print(sprout[:sprout.rfind("\n")])
         else:
             print(sph)
-    
-    elif fil == "_na": print("No scripts were generated")
-    else: print(f"No file \"{fil}\" found")
+    elif fil == "_na":
+        print("No scripts were generated")
+    else:
+        print(f"No file \"{fil}\" found")
     
     
